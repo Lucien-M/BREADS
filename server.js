9b20e6bc80b5e7bb8f3a236143a36dbfe2b72ca4
Lucien-M <mail@gmail.com>
2022-09-28 19:39:41 -0400

    some update

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
-
+//DEPENDENCIES
 const express = require('express')
-require('dotenv').config()
-const PORT = process.env.PORT
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Bread!')
+//CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+
+//ROUTES
+app.get('/', (req, res) =>{
+    res.send('Welcome to an Awesome App about Breads!')
 })
 
+//BREADS
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
-
+//LISTEN
 app.listen(PORT, () => {
-    console.log('listening on port', PORT);
+    console.log('LISTENING ON SERVER', PORT);
 })
